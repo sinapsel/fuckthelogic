37f8b77eed69b2c650caa3b98c67b4e3c9ad46a5
Sinapsel <marksimanovskiy@gmail.com>
2017-06-13 00:10:00 +0300

    Added IF..ELSE constucrions
    Added IF..ELSE constructions, maximum value and iterations checks

--- fuckthelogic.py
+++ fuckthelogic.py
@@ -8,7 +8,7 @@ import sys
 class Inpt:
     
     def filtr(self,source):
-        return ''.join(c for c in source if c in '><+-!?{};:/\\~@')
+        return ''.join(c for c in source if c in '><+-!?{};:/~@()\\')
     
     def flowrun(self):
         Syntaxhelp()
@@ -17,11 +17,11 @@ class Inpt:
                 source = input()
                 code = Interpreter(source)
                 code.run()
-            except (KeyboardInterrupt, SystemExit, KeyError, IndexError, ValueError, EOFError, OverflowError):
+            except (KeyboardInterrupt, SystemExit, KeyError, IndexError, ValueError, EOFError, OverflowError, Exception):
                 print("\n\n"+"-"*8+"\nLogic was unsuccessfully fucked with error code 666")
                 del code
                 exit(666)
-
+                
     def filerun(self,srcpath):
         try:
             filesource = open(srcpath, "r")
@@ -37,8 +37,10 @@ class Inpt:
 class Interpreter:
     
     def __init__(self, sourceToInterpret):
-        print(Inpt.filtr(self, sourceToInterpret))
+        #print(Inpt.filtr(self, sourceToInterpret))
         self.source = Inpt.filtr(self, sourceToInterpret)
+        self.max_val = 2**16
+        self.IterCounter = 2**14
               
     def getLoops(self):
         opened = []
@@ -50,12 +52,36 @@ class Interpreter:
                 loops[i] = opened[-1]
                 loops[opened.pop()] = i
         return loops
+        
+    def getConditions(self):
+        opened = []
+        conds = {}
+        for i in range(len(self.source)):
+            if self.source[i] == '(':
+                opened.append(i)
+            elif self.source[i] == ')':
+                conds[i] = opened[-1]
+                conds[opened.pop()] = i
+        return conds
+    
+    def getElses(self):
+        opened = []
+        elses = {}
+        for i in range(len(self.source)):
+            if self.source[i] == ')':
+                opened.append(i)
+            elif self.source[i] == '\\':
+                elses[i] = opened[-1]
+                elses[opened.pop()] = i
+        return elses
 
     def run(self):
         (x,i) = (0,0)
         FuckTheLogicdict = {0: 0}
-        blocks = self.getLoops()
-        while i < len(self.source):
+        LoopBlocks = self.getLoops()
+        CondBlocks = self.getConditions()
+        ElseBlocks = self.getElses()
+        while (i < len(self.source)):
             __symbol = self.source[i]
             if __symbol == '>':
                 x += 1
@@ -72,21 +98,27 @@ class Interpreter:
                 FuckTheLogicdict[x] = FuckTheLogicdict[x] >> 1
             elif __symbol == '/':
                 FuckTheLogicdict[x] = 1 << FuckTheLogicdict[x]
-            elif __symbol == "\\":
-                FuckTheLogicdict[x] = 0
             elif __symbol == "~":
                 FuckTheLogicdict[x] = FuckTheLogicdict[x-1]
             elif __symbol == '!':
                 print(chr(FuckTheLogicdict[x]), end = "")
             elif __symbol == '@':
-                print(hex(FuckTheLogicdict[x])[2::].upper(), end = "")
+                print(hex(FuckTheLogicdict[x])[2::].upper(), end = "\n")
             elif __symbol == '?':
                 FuckTheLogicdict[x] = int(input('\t\t>'))
             elif __symbol == '{':
-                if not FuckTheLogicdict[x]: i = blocks[i]
+                if not FuckTheLogicdict[x]: i = LoopBlocks[i]
             elif __symbol == '}':
-                if FuckTheLogicdict[x]: i = blocks[i]
+                if FuckTheLogicdict[x]: i = LoopBlocks[i]
+            elif __symbol == '(':
+                if not FuckTheLogicdict[x]: i = CondBlocks[i]
+            elif __symbol == ')':
+                if FuckTheLogicdict[x]: i = ElseBlocks[i]
             i += 1
+            FuckTheLogicdict[x] %= self.max_val
+            self.IterCounter -= 1
+            if (self.IterCounter<1):
+                raise Exception("Overflow")
 
 
 Syntaxhelp = lambda: print("""Here is a simple instuction for FuckTheLogic interpreter:
@@ -101,8 +133,13 @@ Syntaxhelp = lambda: print("""Here is a simple instuction for FuckTheLogic inter
 ; shifts the bits at the data pointer left by one (mul by 2)
 : shifts the bits at the data pointer right by one (div by 2)
 / shifts the bit 1 left by the value at the data pointer (2 in the power)
-\ makes byte at the data pointer equals to 0
-
+{ opens loop while the value at the data pointer is not 0
+} closes loop
+( starts condition if the value at the data pointer is not 0
+) closes condition and opens else condition
+\ closes else condition, always must exist after closing condition
+  For reference: if you need if(x) statement use (code)\, if if(not x) - ()code\, if if(x){code}else{code2} - (code)code2\ 
+  
 
 """)       
 
